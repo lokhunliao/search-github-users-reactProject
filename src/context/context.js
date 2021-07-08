e541d6af1b845001fe7eea4d5483530fc3e42db4
lokhunliao <lokhunnnn@gmail.com>
2021-07-08 11:56:47 -0700

    context API

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,17 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+
+const GithubProvider = ({children}) => {
+    return ( <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>);
+}
+
+//custom hook
+// export const useGlobalContext = () => {
+//     return useContext(GithubContext)
+// }  
+
+
+export {GithubContext, GithubProvider};
